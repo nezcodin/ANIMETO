61cd3e23abc8f6f3161bf86e95fe4464346cbdcb
nezcodin <ch.fusco104@gmail.com>
2023-06-01 18:15:28 -0500

    api data properly assigned, info appends to screen

--- animeto-project/components/SearchBar.tsx
+++ animeto-project/components/SearchBar.tsx
@@ -2,20 +2,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface AnimeResult {
+  id: number
   title: string
   episodes: number
 }
 
 export default function SearchBar() {
 
-  const [animeData, setAnimeData] = useState<AnimeResult[]>([])
+  const [animeResults, setAnimeResults] = useState<AnimeResult[]>([])
 
   useEffect(() => {
     const getResults = async () => {
       try {
         const { data } = await axios.get('https://api.jikan.moe/v4/anime?q=naruto&sfw')
-        console.log(data)
-        setAnimeData(data)
+        setAnimeResults(data.data)
       } catch (error) {
         console.log('There was an error! SearchBar.tsx')
       }
@@ -23,8 +23,26 @@ export default function SearchBar() {
     getResults()
   }, [])
 
+  console.log(animeResults)
+
   return (
-    <div></div>
+    <div>
+      {animeResults.map(animeResult => (
+        <div
+          key={animeResult.id}
+        >
+          <p>{animeResult.title}</p>
+        </div>
+      ))}
+    </div>
   )
 
 }
+
+// set input value to a variable -> searchValue
+// https://api.jikan.moe/v4/anime?q={searchValue}&sfw 
+    // i think sfw should be on by default and can be switched off in the filters
+// show results on screen (need to map it) and console log it
+
+
+// let searchValue = input.value
