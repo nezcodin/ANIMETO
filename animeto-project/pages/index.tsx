4ea7e377de28c5a935077fc8eaf5517105f07d06
nezcodin <ch.fusco104@gmail.com>
2023-06-01 17:31:47 -0500

    set up some colors in tw config

--- animeto-project/pages/index.tsx
+++ animeto-project/pages/index.tsx
@@ -2,7 +2,7 @@ import SearchBar from "@/components/SearchBar"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-bg h-screen w-screen">
       <h1>ANIMETO</h1>
       <SearchBar></SearchBar>
     </div>
