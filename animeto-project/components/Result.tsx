7494af7e7e9983014cd752e8421eee81ad73dcde
nezcodin <ch.fusco104@gmail.com>
2023-06-04 14:50:16 -0500

    added loading + timeout messages

--- animeto-project/components/Result.tsx
+++ animeto-project/components/Result.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 export default function Result() {
 
   const [animeResult, setAnimeResult] = useState<AnimeResult | null>(null);
+  const [showTimeoutMessage, setShowTimeoutMessage] = useState(false)
 
   // this is grabbing the mal_id from the url
   const router = useRouter();
@@ -16,25 +17,34 @@ export default function Result() {
       try {
         const { data } = await axios.get(
           `https://api.jikan.moe/v4/anime/${mal_id}`
-        );
-        setAnimeResult(data);
+        )
+        setAnimeResult(data)
       } catch (error) {
-        console.log("There was an error! Result.tsx");
+        console.log("There was an error! Result.tsx")
       }
-    };
+    }
 
-    fetchAnimeResult();
-  }, [mal_id]);
+    if (mal_id) {
+      fetchAnimeResult()
+    }
+
+    const timeout = setTimeout(() => {
+      setShowTimeoutMessage(true)
+    }, 10_000)
+
+    return () => clearTimeout(timeout)
+
+  }, [mal_id])
 
   console.log(animeResult)
+
+  if (showTimeoutMessage) {
+    return <p>Sorry, this page has timed out. Please go back to the previous page.</p>;
+  }
 
   if (!animeResult) {
     return <p>Loading...</p>;
   }
-
-  // if (setTimeout(() => {
-  //   return <p>Please refresh the page or go back.</p>
-  // }, 10_000)) <- show this if !AnimeResult > 10s
 
   return (
     <div>
